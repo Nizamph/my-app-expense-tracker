562773be117024b8ea043e6ce5dc8d75c5b8fb22
Nizamph <116118003+Nizamph@users.noreply.github.com>
2023-02-17 23:23:49 +0530

    Expense Added to the backend

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,31 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Expense.module.css'
 import ExpenseList from './ExpenseList'
 import { useRef } from 'react'
+import axios from 'axios'
+import { json } from 'react-router-dom'
 const ExpenseForm = () => {
   const expenseInputRef = useRef();
   const desciptionInputRef = useRef();
   const categoryInputRef = useRef();
   
   const[expense,setExpense] = useState([])
+  
   const expenseSubmitHandler = (event) => {
+    event.preventDefault()
     const enteredExpense = expenseInputRef.current.value;
     const enteredDescription = desciptionInputRef.current.value;
     const selectedCategory = categoryInputRef.current.value;
-    console.log(enteredDescription,enteredExpense,selectedCategory)
-      event.preventDefault()
+    const postData = {
+      amount:enteredExpense,
+      description:enteredDescription,
+      category:selectedCategory,
+      id:Math.random().toString()
+    }
+    axios.post("https://expense-tracker-c0524-default-rtdb.firebaseio.com/expenses.json",postData)
+    .then((res) => {
+      console.log(res)
       setExpense((prevState) => {
         return [...prevState,{amount:enteredExpense,description:enteredDescription,category:selectedCategory,id:Math.random().toString()}]
       })
+    }).catch((err) => {
+      console.log(err)
+    })
   }
-  console.log(expense)
+
+  useEffect(() => {
+    axios.get("https://expense-tracker-c0524-default-rtdb.firebaseio.com/expenses.json")
+    .then((res) => {
+      console.log(res.data)
+       setExpense(Object.values(res.data))
+    }).catch((err) => {
+      console.log(err)
+    }) 
+  },[])
   return (
     <React.Fragment>
 <div className="container shadow p-4">
   <form className="row g-3" onSubmit={expenseSubmitHandler}>
     <div className="col-md-4">
       <label  className="form-label">Expense</label>
-      <input type="text" className="form-control" placeholder="Enter expense" ref={expenseInputRef}/>
+      <input type="text" className="form-control" placeholder="Enter expense" ref={expenseInputRef} required/>
     </div>
     <div className="col-md-4">
       <label  className="form-label">Description</label>
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,11 +3,13 @@ import { Card } from 'react-bootstrap';
 import styles from "./Expense.module.css"
 
 const ExpenseList = (props) => {
+    console.log(props.expenseList)
   return (
     <Card className={styles.expenses}>
        <Card.Title className='m-auto pt-3 border-bottom  pb-3'>Expense Details</Card.Title>
         <ul>
           {props.expenseList.map( item => (
+           
             <li key={item.id}>
               <div className={styles.expenseItem}>
               <p><b>Expense:&nbsp;&nbsp;</b>{item.amount}</p> 
